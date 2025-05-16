578987d75bf786d76c6b2a3fbe3ca316088ff05f
kido0127 <k024g0012@g.neec.ac.jp>
2025-05-16 16:37:17 +0900

    カメラ機能とテンプレートを追加。後々関数などにする

--- main.cpp
+++ main.cpp
@@ -10,6 +10,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
     // ライブラリの初期化
     Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
+    Vector3 cameraTranslate = { 0.0f, 1.9f, -6.49f };
+    Vector3 cameraRotate = { 0.26f, 0.0f, 0.0f };
+    uint32_t color = WHITE;
 
     // キー入力結果を受け取る箱
     char keys[256] = { 0 };
@@ -27,6 +30,30 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
         ///
         /// ↓更新処理ここから
         ///
+#pragma region カメラ 
+        // カメラ行列を作成
+        Matrix4x4 cameraMatrix = MakeAffineMatrix(
+            { 1.0f, 1.0f, 1.0f }, // スケール
+            cameraRotate,      // 回転
+            cameraTranslate        // 平行移動
+        );
+
+        // ビュー行列を計算（カメラ行列の逆行列）
+        Matrix4x4 viewMatrix = Inverse(cameraMatrix);
+
+        // プロジェクション行列を作成
+        Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(
+            0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f
+        );
+
+        // ビュー・プロジェクション行列を合成
+        Matrix4x4 viewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
+
+        // ビューポート行列を作成
+        Matrix4x4 viewportMatrix = MakeViewportMatrix(
+            0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f
+        );
+#pragma endregion
 
         
 
@@ -38,6 +65,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
         /// ↓描画処理ここから
         ///
         ImGui::Begin("Window");
+        ImGui::DragFloat3("CameraTranslate", &cameraTranslate.x, 0.01f);
+        ImGui::DragFloat3("CameraRotate", &cameraRotate.x, 0.01f);
 		ImGui::End();
 
         ///

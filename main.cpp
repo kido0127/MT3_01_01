71685d513befa449c2015a9379fd95fa762dcae8
kido0127 <k024g0012@g.neec.ac.jp>
2025-05-23 11:25:15 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -98,6 +98,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
         // フレームの終了
         Novice::EndFrame();
 
+
         // ESCキーが押されたらループを抜ける
         if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
             break;
